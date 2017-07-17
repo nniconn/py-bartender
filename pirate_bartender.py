1c2ca1c7c49122d1760d54da3c8e2201b9bef9fd
Nicole Nguyen <nicole.nguyen321@gmail.com>
2017-07-17 18:58:53 +0000

    user input

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -14,8 +14,45 @@ ingredients = {"strong": ["glug of rum", "slug of whisky", "splash of gin"],
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
-for k,v in questions.items():
-    print(v)
-    my_input=input("yes or no:  ")
-    print(my_input)
 
+# for k,v in questions.items():
+    # print(v)
+    # my_input=input("yes or no:  ")
+    # print(my_input)
+    
+
+# def get_bool(prompt):
+#     while True: 
+#         try:
+#           return {"true":True, "false":False}[input(prompt).lower()]
+#         except KeyError:
+#             print("Invalid input, please Enter true or false.")
+            
+#     print get_bool ("Do ye like yer drinks strong?")
+
+
+
+
+
+# for key in questions:
+#     print (questions[key])
+
+
+
+# for key in questions:
+#     response = False if input("Yes or No:  ").lower() == 'no' else True
+#     print (response(questions[key]))
+
+
+
+
+# def drink(questions, default="no"):
+#     input = {"yes": True, "or" "no": False}
+# while True:
+#     for key in questions:
+#         print(questions[key]), input("yes or no : ")
+
+
+
+for key in questions:
+    print(questions[key]), input("yes or no : ")
